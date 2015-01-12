9a9247e94ef9b95313b510ee8a0aa23da592434f
pawankumarthakur <surpawan@gmail.com>
2015-01-12 11:53:34 +0530

    Created Basic Homepage

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Welcome Site </title>
+	<title> Blogger </title>
+	<link rel="shortcut icon" href="<?php echo base_url()?>favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/style.css">
 	<script type="text/javascript" src="<?php echo base_url()?>js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Blog</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Username" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
 
